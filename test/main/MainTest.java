a92bdfe06c2dfe03f2f203aa46259e8ea93a80d7
Subhajit Sahu <wolfram77@gmail.com>
2015-04-05 18:16:54 +0530

    JUnit Test is running locally

--- test/main/MainTest.java
+++ test/main/MainTest.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// @wolfram77
 package main;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.*;
 
-/**
- *
- * @author wolfram77
- */
+
+
 public class MainTest {
     
     public MainTest() {
@@ -36,17 +25,14 @@ public class MainTest {
     @After
     public void tearDown() {
     }
-
-    /**
-     * Test of main method, of class Main.
-     */
+    
+    
     @Test
     public void testMain() {
-        System.out.println("main");
+        System.out.println("test - main");
         String[] args = null;
         Main.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("\n");
     }
     
 }
